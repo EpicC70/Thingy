b2ba132581dcc64e4c9d35fe4459b0a5cb3aa5d9
EpicC70 <ahinkley@student.fitchburgstate.edu>
2023-03-27 15:21:30 -0400

    Added Code

---
+++ Assets/Scripts/WayPoints.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform GetWaypoint(int waypointIndex)
+    {
+        return transform.GetChild(waypointIndex);
+    }
+
+    // Update is called once per frame
+    public int GetNextWaypointIndex(int currentWaypointIndex)
+    {
+        int nextWaypointIndex = currentWaypointIndex + 1;
+
+        if (nextWaypointIndex == transform.childCount)
+        {
+            nextWaypointIndex = 0;
+        }
+
+        return nextWaypointIndex;
+    }
+
+
+}
